463f586e0225a5a4c5706c99d4dbc2feb7e09bee
Maxime Brochot <brochot.max@gmail.com>
2026-08-15 15:15:11 +0200

    data: add Prophecies Warrior armor dataset

--- src/data/armors.js
+++ src/data/armors.js
@@ -25,14 +25,210 @@ const armors = {
       },
     },
     {
-      id: "warrior-factions-sample",
-      name: "Factions Armor",
+      id: "warrior-charr-hide-armor",
+      name: "Charr Hide Armor",
       professionId: "warrior",
-      campaignId: "factions",
-      location: "To be defined",
+      campaignId: "prophecies",
+      location: "Droknar's Forge",
       images: {
-        male: null,
-        female: null,
+        male: "/images/armors/warrior/Warrior_Charr_Hide_armor_m.jpg",
+        female: "/images/armors/warrior/Warrior_Charr_Hide_armor_f.jpg",
+      },
+      cost: {
+        gold: 25000,
+        materials: [
+          {
+            materialId: "iron-ingot",
+            quantity: 200,
+          },
+          {
+            materialId: "fur-square",
+            quantity: 32,
+          },
+        ],
+      },
+    },
+    {
+      id: "warrior-gladiator-armor",
+      name: "Gladiator Armor",
+      professionId: "warrior",
+      campaignId: "prophecies",
+      location: "Droknar's Forge",
+      images: {
+        male: "/images/armors/warrior/Warrior_Gladiator_armor_m.jpg",
+        female: "/images/armors/warrior/Warrior_Gladiator_armor_f.jpg",
+      },
+      cost: {
+        gold: 25000,
+        materials: [
+          {
+            materialId: "iron-ingot",
+            quantity: 200,
+          },
+          {
+            materialId: "tanned-hide-square",
+            quantity: 128,
+          },
+        ],
+      },
+    },
+    {
+      id: "warrior-wyvern-armor",
+      name: "Wyvern Armor",
+      professionId: "warrior",
+      campaignId: "prophecies",
+      location: "Droknar's Forge",
+      images: {
+        male: "/images/armors/warrior/Warrior_Wyvern_armor_m.jpg",
+        female: "/images/armors/warrior/Warrior_Wyvern_armor_f.jpg",
+      },
+      cost: {
+        gold: 25000,
+        materials: [
+          {
+            materialId: "iron-ingot",
+            quantity: 200,
+          },
+          {
+            materialId: "scale",
+            quantity: 32,
+          },
+        ],
+      },
+    },
+    {
+      id: "warrior-platemail-armor",
+      name: "Platemail Armor",
+      professionId: "warrior",
+      campaignId: "prophecies",
+      location: "Droknar's Forge",
+      images: {
+        male: "/images/armors/warrior/Warrior_Platemail_armor_m.jpg",
+        female: "/images/armors/warrior/Warrior_Platemail_armor_f.jpg",
+      },
+      cost: {
+        gold: 25000,
+        materials: [
+          {
+            materialId: "iron-ingot",
+            quantity: 200,
+          },
+          {
+            materialId: "steel-ingot",
+            quantity: 32,
+          },
+        ],
+      },
+    },
+    {
+      id: "warrior-templar-armor",
+      name: "Templar Armor",
+      professionId: "warrior",
+      campaignId: "prophecies",
+      location: "Droknar's Forge",
+      images: {
+        male: "/images/armors/warrior/Warrior_Templar_armor_m.jpg",
+        female: "/images/armors/warrior/Warrior_Templar_armor_f.jpg",
+      },
+      cost: {
+        gold: 25000,
+        materials: [
+          {
+            materialId: "iron-ingot",
+            quantity: 200,
+          },
+          {
+            materialId: "leather-square",
+            quantity: 28,
+          },
+          {
+            materialId: "steel-ingot",
+            quantity: 4,
+          },
+        ],
+      },
+    },
+    {
+      id: "warrior-elite-charr-hide-armor",
+      name: "Elite Charr Hide Armor",
+      professionId: "warrior",
+      campaignId: "prophecies",
+      location: "Marhan's Grotto",
+      images: {
+        male: "/images/armors/warrior/Warrior_Elite_Charr_Hide_armor_m.jpg",
+        female: "/images/armors/warrior/Warrior_Elite_Charr_Hide_armor_f.jpg",
+      },
+      cost: {
+        gold: 75000,
+        materials: [
+          {
+            materialId: "iron-ingot",
+            quantity: 400,
+          },
+          {
+            materialId: "fur-square",
+            quantity: 40,
+          },
+        ],
+      },
+    },
+    {
+      id: "warrior-elite-gladiator-armor",
+      name: "Elite Gladiator Armor",
+      professionId: "warrior",
+      campaignId: "prophecies",
+      location: "Marhan's Grotto",
+      images: {
+        male: "/images/armors/warrior/Warrior_Elite_Gladiator_armor_m.jpg",
+        female: "/images/armors/warrior/Warrior_Elite_Gladiator_armor_f.jpg",
+      },
+      cost: {
+        gold: 75000,
+        materials: [
+          {
+            materialId: "iron-ingot",
+            quantity: 400,
+          },
+          {
+            materialId: "tanned-hide-square",
+            quantity: 128,
+          },
+        ],
+      },
+    },
+    {
+      id: "warrior-elite-dragon-armor",
+      name: "Elite Dragon Armor",
+      professionId: "warrior",
+      campaignId: "prophecies",
+      location: "The Granite Citadel",
+      images: {
+        male: "/images/armors/warrior/Warrior_Elite_Dragon_armor_m.jpg",
+        female: "/images/armors/warrior/Warrior_Elite_Dragon_armor_f.jpg",
+      },
+      cost: {
+        gold: 75000,
+        materials: [
+          {
+            materialId: "iron-ingot",
+            quantity: 400,
+          },
+          {
+            materialId: "scale",
+            quantity: 40,
+          },
+        ],
+      },
+    },
+    {
+      id: "warrior-elite-platemail-armor",
+      name: "Elite Platemail Armor",
+      professionId: "warrior",
+      campaignId: "prophecies",
+      location: "The Granite Citadel",
+      images: {
+        male: "/images/armors/warrior/Warrior_Elite_Platemail_armor_m.jpg",
+        female: "/images/armors/warrior/Warrior_Elite_Platemail_armor_f.jpg",
       },
       cost: {
         gold: 75000,
@@ -44,6 +240,34 @@ const armors = {
           {
             materialId: "steel-ingot",
             quantity: 40,
+          },
+        ],
+      },
+    },
+    {
+      id: "warrior-elite-templar-armor",
+      name: "Elite Templar Armor",
+      professionId: "warrior",
+      campaignId: "prophecies",
+      location: "Marhan's Grotto",
+      images: {
+        male: "/images/armors/warrior/Warrior_Elite_Templar_armor_m.jpg",
+        female: "/images/armors/warrior/Warrior_Elite_Templar_armor_f.jpg",
+      },
+      cost: {
+        gold: 75000,
+        materials: [
+          {
+            materialId: "iron-ingot",
+            quantity: 400,
+          },
+          {
+            materialId: "deldrimor-steel-ingot",
+            quantity: 5,
+          },
+          {
+            materialId: "elonian-leather-square",
+            quantity: 35,
           },
         ],
       },
--- src/data/craftingRecipes.js
+++ src/data/craftingRecipes.js
@@ -1,5 +1,97 @@
 const craftingRecipes = [
   {
+    id: "bolt-of-damask-crafting",
+    outputMaterialId: "bolt-of-damask",
+    outputQuantity: 1,
+    ingredients: [
+      {
+        materialId: "plant-fiber",
+        quantity: 5,
+      },
+      {
+        materialId: "pile-of-glittering-dust",
+        quantity: 5,
+      },
+    ],
+    gold: 200,
+  },
+  {
+    id: "bolt-of-linen-crafting",
+    outputMaterialId: "bolt-of-linen",
+    outputQuantity: 1,
+    ingredients: [
+      {
+        materialId: "plant-fiber",
+        quantity: 5,
+      },
+    ],
+    gold: 200,
+  },
+  {
+    id: "bolt-of-silk-crafting",
+    outputMaterialId: "bolt-of-silk",
+    outputQuantity: 1,
+    ingredients: [
+      {
+        materialId: "bolt-of-cloth",
+        quantity: 10,
+      },
+      {
+        materialId: "pile-of-glittering-dust",
+        quantity: 10,
+      },
+    ],
+    gold: 250,
+  },
+  {
+    id: "deldrimor-steel-ingot-crafting",
+    outputMaterialId: "deldrimor-steel-ingot",
+    outputQuantity: 1,
+    ingredients: [
+      {
+        materialId: "iron-ingot",
+        quantity: 10,
+      },
+      {
+        materialId: "lump-of-charcoal",
+        quantity: 1,
+      },
+      {
+        materialId: "pile-of-glittering-dust",
+        quantity: 5,
+      },
+    ],
+    gold: 200,
+  },
+  {
+    id: "elonian-leather-square-crafting",
+    outputMaterialId: "elonian-leather-square",
+    outputQuantity: 1,
+    ingredients: [
+      {
+        materialId: "tanned-hide-square",
+        quantity: 5,
+      },
+      {
+        materialId: "pile-of-glittering-dust",
+        quantity: 5,
+      },
+    ],
+    gold: 50,
+  },
+  {
+    id: "leather-square-crafting",
+    outputMaterialId: "leather-square",
+    outputQuantity: 1,
+    ingredients: [
+      {
+        materialId: "tanned-hide-square",
+        quantity: 5,
+      },
+    ],
+    gold: 50,
+  },
+  {
     id: "lump-of-charcoal-crafting",
     outputMaterialId: "lump-of-charcoal",
     outputQuantity: 1,
@@ -12,20 +104,92 @@ const craftingRecipes = [
     gold: 200,
   },
   {
+    id: "roll-of-parchment-crafting",
+    outputMaterialId: "roll-of-parchment",
+    outputQuantity: 1,
+    ingredients: [
+      {
+        materialId: "wood-plank",
+        quantity: 5,
+      },
+    ],
+    gold: 20,
+  },
+  {
+    id: "roll-of-vellum-crafting",
+    outputMaterialId: "roll-of-vellum",
+    outputQuantity: 1,
+    ingredients: [
+      {
+        materialId: "wood-plank",
+        quantity: 5,
+      },
+      {
+        materialId: "pile-of-glittering-dust",
+        quantity: 5,
+      },
+    ],
+    gold: 20,
+  },
+  {
+    id: "spiritwood-plank-crafting",
+    outputMaterialId: "spiritwood-plank",
+    outputQuantity: 1,
+    ingredients: [
+      {
+        materialId: "wood-plank",
+        quantity: 5,
+      },
+      {
+        materialId: "pile-of-glittering-dust",
+        quantity: 10,
+      },
+    ],
+    gold: 100,
+  },
+  {
     id: "steel-ingot-crafting",
     outputMaterialId: "steel-ingot",
     outputQuantity: 1,
     ingredients: [
       {
-        materialId: "lump-of-charcoal",
-        quantity: 1,
-      },
-      {
         materialId: "iron-ingot",
         quantity: 10,
       },
+      {
+        materialId: "lump-of-charcoal",
+        quantity: 1,
+      },
     ],
     gold: 200,
+  },
+  {
+    id: "tempered-glass-vial-crafting",
+    outputMaterialId: "tempered-glass-vial",
+    outputQuantity: 1,
+    ingredients: [
+      {
+        materialId: "pile-of-glittering-dust",
+        quantity: 5,
+      },
+    ],
+    gold: 20,
+  },
+  {
+    id: "vial-of-ink-crafting",
+    outputMaterialId: "vial-of-ink",
+    outputQuantity: 1,
+    ingredients: [
+      {
+        materialId: "plant-fiber",
+        quantity: 4,
+      },
+      {
+        materialId: "tempered-glass-vial",
+        quantity: 1,
+      },
+    ],
+    gold: 20,
   },
 ];
 
