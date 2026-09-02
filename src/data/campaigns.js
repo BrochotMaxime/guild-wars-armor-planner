c1329efffeb6c73a3d51717303f7774840f62a8a
BrochotMaxime <brochot.max@gmail.com>
2026-09-02 16:34:26 +0200

    feat: add profession and campaign selection assets

--- src/data/campaigns.js
+++ src/data/campaigns.js
@@ -1,9 +1,29 @@
 const campaigns = [
-  { id: "prophecies", name: "Prophecies" },
-  { id: "factions", name: "Factions" },
-  { id: "nightfall", name: "Nightfall" },
-  { id: "eye-of-the-north", name: "Eye of the North" },
-  { id: "core", name: "Core" },
+  {
+    id: "prophecies",
+    name: "Prophecies",
+    image: "/images/campaigns/prophecies.webp",
+  },
+  {
+    id: "factions",
+    name: "Factions",
+    image: "/images/campaigns/factions.webp",
+  },
+  {
+    id: "nightfall",
+    name: "Nightfall",
+    image: "/images/campaigns/nightfall.webp",
+  },
+  {
+    id: "eye-of-the-north",
+    name: "Eye of the North",
+    image: "/images/campaigns/eye-of-the-north.webp",
+  },
+  {
+    id: "core",
+    name: "Core",
+    image: "/images/campaigns/fissure-of-woe.webp",
+  },
 ];
 
 export default campaigns;
--- src/data/professions.js
+++ src/data/professions.js
@@ -1,14 +1,64 @@
 const professions = [
-  { id: "warrior", name: "Warrior" },
-  { id: "ranger", name: "Ranger" },
-  { id: "monk", name: "Monk" },
-  { id: "necromancer", name: "Necromancer" },
-  { id: "mesmer", name: "Mesmer" },
-  { id: "elementalist", name: "Elementalist" },
-  { id: "assassin", name: "Assassin" },
-  { id: "ritualist", name: "Ritualist" },
-  { id: "paragon", name: "Paragon" },
-  { id: "dervish", name: "Dervish" },
+  {
+    id: "warrior",
+    name: "Warrior",
+    image: "/images/professions/characters/warrior.webp",
+    icon: "/images/professions/icons/warrior.png",
+  },
+  {
+    id: "ranger",
+    name: "Ranger",
+    image: "/images/professions/characters/ranger.webp",
+    icon: "/images/professions/icons/ranger.png",
+  },
+  {
+    id: "monk",
+    name: "Monk",
+    image: "/images/professions/characters/monk.webp",
+    icon: "/images/professions/icons/monk.png",
+  },
+  {
+    id: "necromancer",
+    name: "Necromancer",
+    image: "/images/professions/characters/necromancer.webp",
+    icon: "/images/professions/icons/necromancer.png",
+  },
+  {
+    id: "mesmer",
+    name: "Mesmer",
+    image: "/images/professions/characters/mesmer.webp",
+    icon: "/images/professions/icons/mesmer.png",
+  },
+  {
+    id: "elementalist",
+    name: "Elementalist",
+    image: "/images/professions/characters/elementalist.webp",
+    icon: "/images/professions/icons/elementalist.png",
+  },
+  {
+    id: "assassin",
+    name: "Assassin",
+    image: "/images/professions/characters/assassin.webp",
+    icon: "/images/professions/icons/assassin.png",
+  },
+  {
+    id: "ritualist",
+    name: "Ritualist",
+    image: "/images/professions/characters/ritualist.webp",
+    icon: "/images/professions/icons/ritualist.png",
+  },
+  {
+    id: "paragon",
+    name: "Paragon",
+    image: "/images/professions/characters/paragon.webp",
+    icon: "/images/professions/icons/paragon.png",
+  },
+  {
+    id: "dervish",
+    name: "Dervish",
+    image: "/images/professions/characters/dervish.webp",
+    icon: "/images/professions/icons/dervish.png",
+  },
 ];
 
 export default professions;
