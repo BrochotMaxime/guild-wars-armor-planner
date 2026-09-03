b8c1287881427fd12a31406a941cb4ba6ed28aa7
BrochotMaxime <brochot.max@gmail.com>
2026-09-03 11:58:48 +0200

    feat: redesign armor selection cards

--- src/components/ArmorList/ArmorList.jsx
+++ src/components/ArmorList/ArmorList.jsx
@@ -2,18 +2,22 @@ import ArmorCard from "../ArmorCard/ArmorCard";
 
 function ArmorList({ armors, onSelectArmor }) {
   return (
-    <section>
+    <section className="armor-list">
       <h2>Choose an armor</h2>
 
-      <div>
-        {armors.map((armor) => (
-          <ArmorCard
-            key={armor.id}
-            armor={armor}
-            onSelectArmor={onSelectArmor}
-          />
-        ))}
-      </div>
+      {armors.length > 0 ? (
+        <div className="armor-list__grid">
+          {armors.map((armor) => (
+            <ArmorCard
+              key={armor.id}
+              armor={armor}
+              onSelectArmor={onSelectArmor}
+            />
+          ))}
+        </div>
+      ) : (
+        <p>No armor is available for this selection.</p>
+      )}
     </section>
   );
 }
